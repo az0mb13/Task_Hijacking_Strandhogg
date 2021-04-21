c39c165a290cf2e9371d0e53fd1d507736b74054
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2021-04-21 23:24:03 +0530

    Updated attacker's code

--- AttackerApp/app/src/main/java/com/zombie/attackerapp/MainActivity.java
+++ AttackerApp/app/src/main/java/com/zombie/attackerapp/MainActivity.java
@@ -15,21 +15,16 @@ import android.widget.TextView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         moveTaskToBack(true);
-
     }
-
 
     @Override
     public void onResume(){
         super.onResume();
         setContentView(R.layout.activity_main);
     }
-
 }
